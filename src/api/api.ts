9a5644daf3da6d40153634050b3219c5e1bd96f5
Ivan Escribano <ivanescribano1998@gmail.com>
2022-11-13 19:32:11 +0100

    feat(search users)

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,16 +4,17 @@ import IRepositories from "src/interfaces/Repository";
 
 const api = axios.create({
   baseURL: "https://api.github.com",
+  headers: {
+    authorization: "token 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",
+  },
 });
 
 export const getUsers = async (data: string): Promise<IUserGithub> => {
   const response = await api.get<IUserGithub>(`/users/${data}`);
-  console.log("response");
   return response.data;
 };
 
 export const getRepositories = async (data: string): Promise<IRepositories> => {
   const response = await api.get<IRepositories>(`/users/${data}/repos`);
-  console.log("response");
   return response.data;
 };
--- src/pages/home/components/UserSearcher.tsx
+++ src/pages/home/components/UserSearcher.tsx
@@ -1,13 +1,41 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { useQuery, UseQueryResult } from "@tanstack/react-query";
 import { getUsers } from "src/api/api";
 import IUserGithub from "src/interfaces/User";
 const UserSearcher = () => {
-  const { data }: UseQueryResult<IUserGithub, Error> = useQuery<
-    IUserGithub,
-    Error
-  >(["users"], () => getUsers("ivan-escribano"));
-  return <div>{JSON.stringify(data)}</div>;
+  const [query, setQuery] = useState<string>("");
+  const [user, setUser] = useState<string>("");
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setUser(query);
+    }, 500);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [query]);
+  const { data, isLoading, isError }: UseQueryResult<IUserGithub, Error> =
+    useQuery<IUserGithub, Error>(["users", user], () => getUsers(user), {
+      enabled: user ? true : false,
+    });
+
+  if (isLoading) {
+    console.log("Is loading");
+  } else if (isError) {
+    console.log("ERROR");
+  }
+  return (
+    <div>
+      <form action="" method="get">
+        <input
+          type="text"
+          className="border"
+          onChange={(e) => setQuery(e.target.value)}
+          value={query}
+        />
+      </form>
+      {data ? JSON.stringify(data) : <p>Not found</p>}
+    </div>
+  );
 };
 
 export default UserSearcher;
